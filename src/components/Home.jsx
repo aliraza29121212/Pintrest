ea55fa8586e4db3ba5a9f96e6db9796b12bd5d97
Ali Raza <aliraza291212@gmail.com>
2023-03-25 20:03:12 +0500

    add

---
+++ src/components/Home.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home_content">
+        <div className="feed_text">
+          <h6>For you</h6>
+        </div>
+        <div className="grid-container">
+          <div className="image_div">
+            <img src="/images/home_img.jpg" alt="" />
+          </div>
+          <div className="image_div">
+            <img src="/images/2.jpg" alt="" />
+          </div>
+          <div className="image_div">
+            <img src="/images/8.jpg" alt="" />
+          </div>
+          <div className="image_div">
+            <img src="/images/7.jpg" alt="" />
+          </div>
+          <div className="image_div">
+            <img src="/images/4.jpg" alt="" />
+          </div>
+          <div className="image_div">
+            <img src="/images/5.jpg" alt="" />
+          </div>
+          <div className="image_div">
+            <img src="/images/6.jpg" alt="" />
+          </div>
+          <div className="image_div">
+            <img src="/images/10.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
